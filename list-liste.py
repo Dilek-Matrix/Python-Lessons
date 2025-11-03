a446cc0a27f168f52a7839d365d9de955cb3528d
Dilek-Matrix <dilekince04@gmail.com>
2025-11-03 17:40:39 +0300

    Add files via upload

---
+++ list-liste.py
@@ -0,0 +1,10 @@
+listem = [34, 26.1, 'Python öğreniyorum']
+print(listem)  # [34, 26.1, 'Python öğreniyorum']
+
+asallar = [2,3,5,7,11,13,17,19]
+
+print("asallar [2] = ", asallar [2])  # asallar [2] =  5
+
+print("asallar [0:3] = ", asallar[0:3])  # asallar [0:3] = [2, 3, 5] 7'yi dahil etmez
+
+print("asallar [5:] = ", asallar[5:])  # asallar [5:] = [13,17,19,]
