52ba981fbd657e729a63ccde6fb59e406cedd183
Dilek-Matrix <dilekince04@gmail.com>
2025-11-03 13:28:56 +0300

    Add files via upload

---
+++ string-veri-tipi.py
@@ -0,0 +1,11 @@
+kursAdi = "Python ile Programlama"
+kursAciklama = 'Güzel bir kurs'
+kursSuresi = "20 saat"
+
+mesaj = "kurs adı: " + kursAdi + " , " + "kurs açıklaması: " + kursAciklama + " , " + "kurs süresi: " + kursSuresi
+# print(mesaj)
+# print(mesaj[-1]) #20 saat son karakter t yazar
+
+# yorum satırını kaldırmak için ctrl + k + u+
+# yorum satırı yapmak için de ctrl + k + c
+print(mesaj[0])  # kurs adındaki " k " karakterini yazar
